d4d126301481458607a7ccd3c87be7281b566b63
Lars Dausch <lars.dausch@gmail.com>
2019-12-13 17:12:35 +0100

    Move some code and add lock test.

---
+++ LockPerformance/LockTime.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace LockPerformance
+{
+    public class LockTime
+    {
+        public long artificialResult;
+        private object lockObject = new object();
+
+        private ReaderWriterLockSlim lockSlim = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
+
+        public void RunStandardLock(long amount)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            artificialResult = 0;
+            for (long i = 0; i < amount; i++)
+            {
+                lock(lockObject)
+                {
+                    artificialResult += i;
+                }
+            }
+            watch.Stop();
+            Console.WriteLine($"Standard Lock needed {watch.ElapsedTicks} ticks.");
+
+        }
+
+        public void RunReaderWriterWithReadLock(long amount)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            artificialResult = 0;
+            for (long i = 0; i < amount; i++)
+            {
+                lockSlim.EnterReadLock();
+                artificialResult += i;
+                lockSlim.ExitReadLock();
+            }
+            watch.Stop();
+            Console.WriteLine($"Slim Read Lock needed {watch.ElapsedTicks} ticks.");
+        }
+
+        public void RunReaderWriterWithWriteLock(long amount)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            artificialResult = 0;
+            for (long i = 0; i < amount; i++)
+            {
+                lockSlim.EnterWriteLock();
+                artificialResult += i;
+                lockSlim.ExitWriteLock();
+            }
+            watch.Stop();
+            Console.WriteLine($"Slim write Lock needed {watch.ElapsedTicks} ticks.");
+
+        }
+    }
+}
--- LockPerformance/Program.cs
+++ LockPerformance/Program.cs
@@ -9,23 +9,46 @@ namespace LockPerformance
     {
         static void Main(string[] args)
         {
+            if (args.Length<1 || !int.TryParse(args[0], out int test) || test<1 || test>2)
+            {
+                Console.WriteLine("At the moment only modes 1 and 2 are supported.");
+                Console.WriteLine("Mode 1 runs a lock time comparison");
+                Console.WriteLine("Mode 2 runs some thread time and deadlock tests.");
+            }
+            else
+            {
+                switch (test)
+                {
+                    case 1:
+                        RunLockTests(args);
+                        break;
+                    case 2:
+                        RunThreadTests(args);
+                        break;
+                }
+            }
+
+
+        }
+
+        private static void RunThreadTests(string[] args)
+        {
             long amountOfWork;
             long noOfThreads;
             Work work = new Work();
-//            WorkWithReaderWriteLock work = new WorkWithReaderWriteLock();
+            //            WorkWithReaderWriteLock work = new WorkWithReaderWriteLock();
 
-            if (!long.TryParse(args[0], out amountOfWork))
+            if (args.Length<2 || !long.TryParse(args[1], out amountOfWork))
             {
                 amountOfWork = 50000;
             }
             work.AmountOfWork = amountOfWork;
-            if (!long.TryParse(args[1], out noOfThreads))
+            if (args.Length <3 || !long.TryParse(args[2], out noOfThreads))
             {
                 noOfThreads = 1000;
             }
 
-            Console.WriteLine(
-                $"Locktest (regular lock). Starting with {noOfThreads} threads and amountOfWork={amountOfWork}");
+            Console.WriteLine($"Locktest (regular lock). Starting with {noOfThreads} threads and amountOfWork={amountOfWork}");
 
             List<Thread> workers = new List<Thread>();
             for (long threads = 0; threads < noOfThreads; threads++)
@@ -37,6 +60,18 @@ namespace LockPerformance
             {
                 worker.Start(work);
             }
+        }
+
+        private static void RunLockTests(string[] args)
+        {
+            if (args.Length<2 || !long.TryParse(args[1], out long amountOfWork))
+            {
+                amountOfWork = 50000;
+            }
+            var lockClass = new LockTime();
+            lockClass.RunStandardLock(amountOfWork);
+            lockClass.RunReaderWriterWithReadLock(amountOfWork);
+            lockClass.RunReaderWriterWithWriteLock(amountOfWork);
         }
 
         public static void DoWork(object x)
